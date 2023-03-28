eafa695b7ebafe4cba2ae5d6623107dac5dd7c0e
dmitrii_gromov_3 <dimchikl2003@gmail.com>
2023-03-28 08:26:33 +0200

    final

---
+++ BlockChain/src/main/java/Block.java
@@ -0,0 +1,57 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    private String data;
+    private long timeStamp;
+    private int nonce;
+
+    //Block Constructor.
+    public Block(String previousHash ) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
+        return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+    public boolean addTransaction(Transaction transaction) {
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
+    }
+
+
+
+}
---
+++ BlockChain/src/main/java/Transaction.java
@@ -0,0 +1,99 @@
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String transactionId;
+    public PublicKey sender;
+    public PublicKey reciepient;
+    public float value;
+    public byte[] signature;
+
+    public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+
+    private static int sequence = 0;
+
+    // Constructor: 
+    public Transaction(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+    public boolean processTransaction() {
+
+        if(verifySignature() == false) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //Gathers transaction inputs (Making sure they are unspent):
+        for(TransactionInput i : inputs) {
+            i.UTXO = Main.UTXOs.get(i.transactionOutputId);
+        }
+
+        //Checks if transaction is valid:
+        if(getInputsValue() < Main.minimumTransaction) {
+            System.out.println("Transaction Inputs too small: " + getInputsValue());
+            System.out.println("Please enter the amount greater than " + Main.minimumTransaction);
+            return false;
+        }
+
+        //Generate transaction outputs:
+        float leftOver = getInputsValue() - value;
+        transactionId = calulateHash();
+        outputs.add(new TransactionOutput( this.reciepient, value,transactionId));
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionId));
+
+
+        for(TransactionOutput o : outputs) {
+            Main.UTXOs.put(o.id , o);
+        }
+
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue;
+            Main.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    public float getInputsValue() {
+        float total = 0;
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue;
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutput o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+
+    private String calulateHash() {
+        sequence++;
+        return StringUtil.applySha256(
+                StringUtil.getStringFromKey(sender) +
+                        StringUtil.getStringFromKey(reciepient) +
+                        Float.toString(value) + sequence
+        );
+    }
+}
